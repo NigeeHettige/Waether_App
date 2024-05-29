9f693e78f4cb8990ac3193d35777577a5d04a40b
Nigee Hettige <nigeehettige20@gmail.com>
2024-05-30 04:12:37 +0530

    Modification 1.1

---
+++ weather_app/lib/bloc/weather_bloc_state.dart
@@ -0,0 +1,15 @@
+part of 'weather_bloc_bloc.dart';
+
+@immutable
+sealed class WeatherBlocState {}
+
+final class WeatherBlocInitial extends WeatherBlocState {}
+
+final class WeatherBlocLoading extends WeatherBlocState{}
+final class WeatherBlocFailure extends WeatherBlocState{}
+final class WeatherBlocSuccess extends WeatherBlocState{
+  final Weather weather;
+  WeatherBlocSuccess(this.weather);
+  @override
+  List<Object> get props =>[weather];
+}
